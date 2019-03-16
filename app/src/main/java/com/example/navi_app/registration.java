28a6e2680c63c93eee5f685394e835c73a2bc93d
Dominic Egginton <eggintod@coventry.ac.uk>
2019-03-16 20:27:48 +0000

    added simple code to deal with UI objects on reg page

--- app/src/main/java/com/example/navi_app/registration.java
+++ app/src/main/java/com/example/navi_app/registration.java
@@ -2,12 +2,30 @@ package com.example.navi_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class registration extends AppCompatActivity {
+
+    EditText edit_username;
+    EditText edit_password;
+    EditText edit_confirm_password;
+    Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        // INIT UI
+        edit_username = (EditText) findViewById(R.id.edit_username);
+        edit_password = (EditText) findViewById(R.id.edit_password);
+        edit_confirm_password = (EditText) findViewById(R.id.edit_confirm_password);
+        btn_register = (Button) findViewById(R.id.btn_register);
+    }
+
+    public void btn_register_clicked(View view) {
+
     }
 }
