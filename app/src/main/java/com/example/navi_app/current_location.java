420663a753e128d99a9d8220330718c3dd4c0680
Dominic Egginton <eggintod@coventry.ac.uk>
2019-03-16 18:57:20 +0000

    added bluetooth code to current location page

--- app/src/main/java/com/example/navi_app/current_location.java
+++ app/src/main/java/com/example/navi_app/current_location.java
@@ -1,8 +1,22 @@
 package com.example.navi_app;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -18,5 +32,179 @@ public class current_location extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_location);
+
+        // INIT Bluetooth
+        this.adapter = BluetoothAdapter.getDefaultAdapter();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        registerReceiver(mReceiver, filter);
+        enableBluetooth();
+    }
+
+    // Define new BroadcastReceiver
+    BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            // Get the received action
+            String action = intent.getAction();
+
+            // If action is equal to the bluetooth device found action
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Found bluetooth node
+
+                // Create new BluetoothDevice object from intent and get device info
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress();
+                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+                BLNode bluetoothObject = new BLNode(deviceHardwareAddress, deviceName, rssi);
+
+                // Add node to list on scanned nodes
+                scannedNodes.add(bluetoothObject);
+
+                // Log node has been found
+                String output = String.valueOf(bluetoothObject.address + " : "+ bluetoothObject.name +" : " + bluetoothObject.rssi);
+                Log.i("BLNode", output);
+            }
+
+            // Log action
+            else {Log.i("Action", action);}
+        }
+    };
+
+    public void enableBluetooth() {
+
+        // This there is no bluetooth adapter
+        if (this.adapter == null) {
+            // Log error
+            Log.e("Bluetooth", "Sorry this device does not support bluetooth :(");
+
+            // Create Alert Dialog
+            AlertDialog alertDialog = new AlertDialog.Builder(menu.this).create();
+            alertDialog.setTitle("Bluetooth not supported");
+            alertDialog.setMessage("Sorry your device doesn't support bluetooth");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Exit App", new DialogInterface.OnClickListener() {
+
+                // Exit button on click event
+                public void onClick(DialogInterface dialog, int which) {
+                    // Exit this dialog
+                    dialog.dismiss();
+                    // Exit App
+                    System.exit(1);
+                }
+
+            });
+            // Show Alert Dialog
+            alertDialog.show();
+        }
+        else {
+            // Check for 'ACCESS_COARSE_LOCATION' permission and request on runtime this is needed for bluetooth scanning
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // Request permission for 'ACCESS_COARSE_LOCATION'
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 99);
+            }
+
+            // Check is bluetooth adapter is already enabled
+            if (!this.adapter.isEnabled()) {
+                // Create new intent to enable bluetooth
+                Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                // Start intent in current activity
+                startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    private void scan() {
+
+        // If adapter is already discovering cancel the discovery
+        if (this.adapter.isDiscovering()) {
+            this.adapter.cancelDiscovery();
+        }
+
+        // Clear scanned nodes list
+        scannedNodes.clear();
+
+        // Start discovery on adapter
+        this.adapter.startDiscovery();
+
+        // Create new delay handler of 10 seconds
+        Handler cancelDiscoveryHandler = new Handler();
+        cancelDiscoveryHandler.postDelayed(new Runnable() {
+
+            public void run() {
+                // cancel the discovery on the adapter
+                adapter.cancelDiscovery();
+            }
+
+        }, SCAN_TIME_BT);
+    }
+
+    private void displayCurrentLocation () {
+        // Scan
+        progressSpinner_CurrentLocation.setVisibility(View.VISIBLE);
+        txt_Current_Building_Level.setVisibility(View.GONE);
+        txt_Current_Location.setVisibility(View.GONE);
+        btn_Info.setVisibility(View.GONE);
+        btn_Navi.setVisibility(View.GONE);
+        edittxt_search.setVisibility(View.GONE);
+        edittxt_search.setText(null);
+        scan();
+
+        // Create new delay handler of 10 seconds
+        Handler getCurrentLocationHandler = new Handler();
+        getCurrentLocationHandler.postDelayed(new Runnable() {
+
+            public void run() {
+                // Get current location object
+                Location currentLocation = ls.getCurrentLocation(scannedNodes);
+
+                // Check for null location
+                if (currentLocation != null) {
+
+                    // Output location details to UI
+                    txt_Current_Building_Level.setText(ls.getCurrentBuilding(currentLocation).name + " - " + ls.getCurrentLevel(currentLocation).name);
+                    //txt_Current_Level.setText(ls.getCurrentLevel(currentLocation).name);
+                    txt_Current_Location.setText(currentLocation.name);
+
+                    // Log Nodes that belong to the location
+                    String output = currentLocation.name + " - {";
+                    for (Node node: currentLocation.nodes) {
+                        output += " " + node.address;
+                    }
+                    output += " }";
+                    Log.i("Current Location", output);
+
+
+                    progressSpinner_CurrentLocation.setVisibility(View.GONE);
+                    txt_Current_Building_Level.setVisibility(View.VISIBLE);
+                    txt_Current_Location.setVisibility(View.VISIBLE);
+                    btn_Info.setVisibility(View.VISIBLE);
+                    btn_Navi.setVisibility(View.VISIBLE);
+                    edittxt_search.setVisibility(View.VISIBLE);
+                }else {
+                    // cant get current location
+                    // Create Alert Dialog
+
+                    AlertDialog alertDialog = new AlertDialog.Builder(menu.this).create();
+                    alertDialog.setTitle("Location System");
+                    alertDialog.setMessage("Your current location could not be found");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Close", new DialogInterface.OnClickListener() {
+
+                        // Exit button on click event
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Exit this dialog
+                            dialog.dismiss();
+
+                        }
+
+                    });
+
+
+                }
+            }
+        }, SCAN_TIME_BT);
     }
 }
