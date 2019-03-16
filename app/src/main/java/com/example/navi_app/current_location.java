0cc4744d80a0bf7eb1a87cef73da0cd1591ad671
Dominic Egginton <eggintod@coventry.ac.uk>
2019-03-16 18:54:17 +0000

    added bluetooth objects to current locatiojn page

--- app/src/main/java/com/example/navi_app/current_location.java
+++ app/src/main/java/com/example/navi_app/current_location.java
@@ -1,9 +1,18 @@
 package com.example.navi_app;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class current_location extends AppCompatActivity {
+
+    // Bluetooth objects
+    private BluetoothAdapter adapter;
+    private static final int REQUEST_ENABLE_BT = 99;
+    private static final int SCAN_TIME_BT = 5000;
+    private final ArrayList<BLNode> scannedNodes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
