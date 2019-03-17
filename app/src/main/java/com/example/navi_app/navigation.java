52979f73e5490ca64dc9d074392f6f046b2c7e37
Dominic Egginton <eggintod@coventry.ac.uk>
2019-03-17 20:41:23 +0000

    added bluetooth code to navi page

--- app/src/main/java/com/example/navi_app/navigation.java
+++ app/src/main/java/com/example/navi_app/navigation.java
@@ -1,26 +1,154 @@
 package com.example.navi_app;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Handler;
 import android.os.TestLooperManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class navigation extends AppCompatActivity {
 
-    TextView txt_test;
+    // Bluetooth objects
+    private BluetoothAdapter adapter;
+    private static final int REQUEST_ENABLE_BT = 99;
+    private static final int SCAN_TIME_BT = 5000;
+    private final ArrayList<BLNode> scannedNodes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
-        //Path navigationPath = (Path) getIntent().getExtras().getSerializable("Path");
+        // INIT Bluetooth
+        this.adapter = BluetoothAdapter.getDefaultAdapter();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        registerReceiver(mReceiver, filter);
+        enableBluetooth();
 
-        txt_test = (TextView) findViewById(R.id.txt_test);
-        //txt_test.setText(navigationPath.convertToString());
+    }
+
+    // Define new BroadcastReceiver
+    BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            // Get the received action
+            String action = intent.getAction();
+
+            // If action is equal to the bluetooth device found action
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Found bluetooth node
+
+                // Create new BluetoothDevice object from intent and get device info
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress();
+                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+                BLNode bluetoothObject = new BLNode(deviceHardwareAddress, deviceName, rssi);
+
+                // Add node to list on scanned nodes
+                scannedNodes.add(bluetoothObject);
+
+                // Log node has been found
+                String output = String.valueOf(bluetoothObject.address + " : "+ bluetoothObject.name +" : " + bluetoothObject.rssi);
+                Log.i("BLNode", output);
+            }
+
+            // Log action
+            else {Log.i("Action", action);}
+        }
+    };
+
+    public void enableBluetooth() {
+
+        // This there is no bluetooth adapter
+        if (this.adapter == null) {
+            // Log error
+            Log.e("Bluetooth", "Sorry this device does not support bluetooth :(");
+
+            // Create Alert Dialog
+            AlertDialog alertDialog = new AlertDialog.Builder(current_location.this).create();
+            alertDialog.setTitle("Bluetooth not supported");
+            alertDialog.setMessage("Sorry your device doesn't support bluetooth");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Exit App", new DialogInterface.OnClickListener() {
+
+                // Exit button on click event
+                public void onClick(DialogInterface dialog, int which) {
+                    // Exit this dialog
+                    dialog.dismiss();
+                    // Exit App
+                    System.exit(1);
+                }
+
+            });
+            // Show Alert Dialog
+            alertDialog.show();
+        }
+        else {
+            // Check for 'ACCESS_COARSE_LOCATION' permission and request on runtime this is needed for bluetooth scanning
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // Request permission for 'ACCESS_COARSE_LOCATION'
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 99);
+            }
+
+            // Check is bluetooth adapter is already enabled
+            if (!this.adapter.isEnabled()) {
+                // Create new intent to enable bluetooth
+                Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                // Start intent in current activity
+                startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    private void scan() {
+
+        // If adapter is already discovering cancel the discovery
+        if (this.adapter.isDiscovering()) {
+            this.adapter.cancelDiscovery();
+        }
+
+        // Clear scanned nodes list
+        scannedNodes.clear();
+
+        // Start discovery on adapter
+        this.adapter.startDiscovery();
+
+        // Create new delay handler of 10 seconds
+        Handler cancelDiscoveryHandler = new Handler();
+        cancelDiscoveryHandler.postDelayed(new Runnable() {
+
+            public void run() {
+                // cancel the discovery on the adapter
+                adapter.cancelDiscovery();
+            }
+
+        }, SCAN_TIME_BT);
+    }
+
+    public void btn_go_clicked(View view) {
 
     }
 }
