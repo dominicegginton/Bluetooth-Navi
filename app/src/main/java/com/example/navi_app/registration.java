7648d0df6ace81f950713d151274ef1facb8f62d
Nicholas Anderson <ander155@coventry.ac.uk>
2019-03-25 23:04:20 +0000

    Added basic registration code

--- app/src/main/java/com/example/navi_app/registration.java
+++ app/src/main/java/com/example/navi_app/registration.java
@@ -1,5 +1,8 @@
 package com.example.navi_app;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +11,7 @@ import android.widget.EditText;
 
 public class registration extends AppCompatActivity {
 
-    EditText edit_username;
+    EditText edit_email;
     EditText edit_password;
     EditText edit_confirm_password;
     Button btn_register;
@@ -19,13 +22,78 @@ public class registration extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         // INIT UI
-        edit_username = (EditText) findViewById(R.id.edit_username);
+        edit_email = (EditText) findViewById(R.id.edit_username);
         edit_password = (EditText) findViewById(R.id.edit_password);
         edit_confirm_password = (EditText) findViewById(R.id.edit_confirm_password);
         btn_register = (Button) findViewById(R.id.btn_register);
     }
 
     public void btn_register_clicked(View view) {
+        String newUserEmail = edit_email.getText().toString();
+        String newUserPassword = edit_password.getText().toString();
+        String newUserConfirmPassword = edit_confirm_password.getText().toString();
 
+        if (!newUserEmail.equals("") || !newUserPassword.equals("") || !newUserConfirmPassword.equals("")) {
+
+            if (newUserPassword.equals(newUserConfirmPassword)){
+                DatabaseHelp databaseHelper = new DatabaseHelp(this);
+                if (databaseHelper.registerUser(newUserEmail, newUserPassword, "Student")) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(registration.this).create();
+                    alertDialog.setTitle("Registration");
+                    alertDialog.setMessage("Your registration was successful");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Logiin", new DialogInterface.OnClickListener() {
+
+                        // Exit button on click event
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Exit this dialog
+                            dialog.dismiss();
+                            // Create new intent to open new page
+                            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                            // Open page
+                            startActivity(intent);
+                        }
+
+                    });
+                    // Show Alert Dialog
+                    alertDialog.show();
+                }
+            }else {
+                AlertDialog alertDialog = new AlertDialog.Builder(registration.this).create();
+                alertDialog.setTitle("Password Error");
+                alertDialog.setMessage("Sorry your passwords do not match");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Try Again", new DialogInterface.OnClickListener() {
+
+                    // Exit button on click event
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Exit this dialog
+                        dialog.dismiss();
+                        edit_password.getText().clear();
+                        edit_confirm_password.getText().clear();
+                    }
+
+                });
+                // Show Alert Dialog
+                alertDialog.show();
+            }
+
+        }else {
+            AlertDialog alertDialog = new AlertDialog.Builder(registration.this).create();
+            alertDialog.setTitle("Registration Error");
+            alertDialog.setMessage("Sorry there was an error while trying to register your details to the databse");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Try Again", new DialogInterface.OnClickListener() {
+
+                // Exit button on click event
+                public void onClick(DialogInterface dialog, int which) {
+                    // Exit this dialog
+                    dialog.dismiss();
+                    edit_email.getText().clear();
+                    edit_password.getText().clear();
+                    edit_confirm_password.getText().clear();
+                }
+
+            });
+            // Show Alert Dialog
+            alertDialog.show();
+        }
     }
 }
