073c4a1e688372aa56496eb4d0b57e3f10528cf5
Dominic Egginton <eggintod@coventry.ac.uk>
2019-03-27 15:53:58 +0000

    added code to init UI elements on extra login pages that are not in use

--- app/src/main/java/com/example/navi_app/staff_login.java
+++ app/src/main/java/com/example/navi_app/staff_login.java
@@ -2,12 +2,26 @@ package com.example.navi_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class staff_login extends AppCompatActivity {
+
+    //UI
+    Button btn_register;
+    Button btn_login;
+    EditText edit_email;
+    EditText edit_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_login);
+
+        // INIT UI
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_register = (Button) findViewById(R.id.btn_register);
+        edit_password = (EditText) findViewById(R.id.edit_password);
+        edit_email = (EditText) findViewById(R.id.edit_email);
     }
 }
